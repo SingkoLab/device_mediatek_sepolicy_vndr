c869f00da776c4e13e57e3312360e1aabc0f24bd
Giovanni Ricca <giovanniricca@proton.me>
2023-12-28 10:35:43 +0000

    sepolicy: Guard invalid labels
    * MTK devices with R vendor and older still depends on those labels
    
    Change-Id: If2e78d5a22722b0038afbb6f9a651bc073b8f4c8

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,12 +1,17 @@
 # Board specific SELinux policy variable definitions
 MTK_SEPOLICY_PATH := device/mediatek/sepolicy_vndr
 
+ifeq ($(BOARD_MTK_SEPOLICY_IS_LEGACY), true)
 # Build with broken namespaces
 # Userspace blobs are still dependent older props that
 # do not pass the VTS test cases.
 BUILD_BROKEN_VENDOR_PROPERTY_NAMESPACE := true
 
-BOARD_SEPOLICY_DIRS += \
+BOARD_VENDOR_SEPOLICY_DIRS += \
+    $(MTK_SEPOLICY_PATH)/legacy/non_plat
+endif
+
+BOARD_VENDOR_SEPOLICY_DIRS += \
     $(MTK_SEPOLICY_PATH)/basic/non_plat \
     $(MTK_SEPOLICY_PATH)/basic/debug/non_plat \
     $(MTK_SEPOLICY_PATH)/bsp/non_plat \
